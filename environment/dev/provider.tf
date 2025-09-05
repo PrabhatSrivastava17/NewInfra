c6524695cc7c99f68ea0c7248e7140455d88582f
PrabhatSrivastava17 <prabhat.sri1701@gmail.com>
2025-09-05 14:55:37 +0530

    Update provider.tf

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,15 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.40.0"
+      source = "hashicorp/azurerm"
+      version = "4.43.0"
     }
   }
-
+}
 
 provider "azurerm" {
-
-  features {}
+feature {}
   subscription_id = "b3b6b7ed-0b15-49ca-b3ce-092e07ca3fef"
 
 }
+
