4744ac956e2f37833d01d2ca3d8c564733fbff73
PrabhatSrivastava17 <prabhat.sri1701@gmail.com>
2025-09-05 17:22:24 +0530

    Update provider.tf

--- environment/prod/provider.tf
+++ environment/prod/provider.tf
@@ -1,20 +1,16 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.40.0"
+      source = "hashicorp/azurerm"
+      version = "4.43.0"
     }
-  }
-  backend "azurerm" {
-    resource_group_name  = "prod2908"
-    storage_account_name = "prodstorage2908"
-    container_name       = "prodcontainer2809" 
-    key                  = "prod.terraform.tfstate"
   }
 }
 
 provider "azurerm" {
-
-  features {}
+features {}
   subscription_id = "b3b6b7ed-0b15-49ca-b3ce-092e07ca3fef"
+
 }
+
+
