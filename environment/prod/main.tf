79d352c8702e10a28f3b6ea39d4130dd773ad7c0
PrabhatSrivastava17 <prabhat.sri1701@gmail.com>
2025-09-05 17:44:53 +0530

    Update main.tf

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -3,11 +3,4 @@ resource "azurerm_resource_group" "tata1" {
   location = "westeurope"
 }
 
-resource "azurerm_storage_account" "prabhatstorageacc" {
-  name                     = "prod-prabhatstorageacc"
-  resource_group_name      = azurerm_resource_group.tata1.name
-  location                 = azurerm_resource_group.tata1.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-}
 
